c6ec5542fcff7ee548f00833cdb44c57984ab07f
Levi <levisnakayama@hotmail.com>
2024-07-08 19:57:46 -0300

    middleware funcs

---
+++ middleware.js
@@ -0,0 +1,36 @@
+const { response } = require('express');
+
+var Middleware = {
+    isAuthenticated: (req, res, next) => {
+        const jwt = require('jsonwebtoken');
+        const token = req.headers['authorization'];
+        if (!token) {
+            return res.status(401).json({ auth: false, message: 'No authorized.' });
+        }
+        jwt.verify(token, process.env.SECRET, function (err, decoded) {
+            if (err) {
+                return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+            }
+            next();
+        });
+    },
+    ok: (res, data = null) => {
+        res.json({
+            response: {
+                code: 0,
+                message: 'success'
+            },
+            data: data
+        });
+    },
+    error: (res, code, message) => {
+        res.status(code).json({
+            response: {
+                code: code,
+                message: message
+            }
+        });
+    }
+};
+
+module.exports = Middleware;
