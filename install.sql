af2b471da89591840a2dc5db1b4fd8bc1581a7d6
Levi <levisnakayama@hotmail.com>
2024-07-08 20:20:35 -0300

    fix sql create

--- install.sql
+++ install.sql
@@ -6,9 +6,9 @@ CREATE TABLE IF NOT EXISTS public."order" (
 );
 
 CREATE TABLE IF NOT EXISTS public.items (
-	orderid uuid NOT NULL,
+	orderid varchar(20) NOT NULL,
 	productid int8 NOT NULL,
 	quantity int8 NOT NULL,
 	price float8 NOT NULL,
-	CONSTRAINT items_pk PRIMARY KEY (orderid, productid)
+	CONSTRAINT item_pk PRIMARY KEY (orderid, productid)
 );
