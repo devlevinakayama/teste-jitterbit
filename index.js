e4b6608e85724645120b496710981af542db1602
Levi <levisnakayama@hotmail.com>
2024-07-08 18:37:31 -0300

    call database

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
 const express = require('express')
 const app = express()
 const port = 3000
+const Database = require('./db')
+const db = new Database();
 
 app.get('/orders', (req, res) => {
   
 })
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Start app listening on port ${port}`)
 })
