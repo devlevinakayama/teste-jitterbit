c33d2f7c4cc703c6d022f9382c61a6fc2f9a772d
Levi <levisnakayama@hotmail.com>
2024-07-08 19:58:10 -0300

    config express

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 const express = require('express')
 const app = express()
 const port = 3000
-const orders = require('./controllers/orders')
 
 app.use(express.json());
-app.use('/orders', orders);
+app.use(express.urlencoded({ extended: true }));
+app.use('/auth', require('./controllers/auth'));
+app.use('/orders', require('./controllers/orders'));
 
 app.listen(port, async () => {
   console.log(`Start app listening on port ${port}`)
